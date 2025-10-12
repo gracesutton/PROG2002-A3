23b96d34dba12d36524557aa6fc0930c0f00cbef
gracesutton <gracesutton20@gmail.com>
2025-10-12 23:53:54 +1000

    Update app.config.ts
    Update app.config to import global modules to work on the standalone components

--- admin/src/app/app.config.ts
+++ admin/src/app/app.config.ts
@@ -3,6 +3,18 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+import { importProvidersFrom } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { provideHttpClient } from '@angular/common/http';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes),
+    
+    // Global modules
+    importProvidersFrom(CommonModule, FormsModule),
+    provideHttpClient()
+  ]
 };
