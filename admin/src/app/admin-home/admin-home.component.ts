68a93b9af533d0b9be2c02940a4a14f8ce20d2ab
gracesutton <gracesutton20@gmail.com>
2025-10-13 13:37:08 +1000

    Admin Home Page
    Finished AdminHomeComponent with welcome message and navigation options with minimal styling for a centred layout and clean admin appearance.

--- admin/src/app/admin-home/admin-home.component.ts
+++ admin/src/app/admin-home/admin-home.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-admin-home',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './admin-home.component.html',
   styleUrl: './admin-home.component.css'
 })
